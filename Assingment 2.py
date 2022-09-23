e79f8012e3d34b0542bc79e78ed160f0b7521ef3
nishokrajanp <75370618+nishokrajanp@users.noreply.github.com>
2022-09-23 17:20:54 +0530

    Update Assingment 2.py

--- Assingment 2.py
+++ Assingment 2.py
@@ -1,4 +1,15 @@
+"""
+IBM-Project-17183-1659630098
+Personal Assistance for Seniors Who Are Self-Reliant
+TITLE:Personal Assistance for Seniors Who Are Self-Reliant
 
+Nishok Rajan P – 917719D060 
+Karthikeyan B – 917719D036 
+Saravana Prakash S – 917719D081 
+Viswa Bharathi K– 917719D112
+
+Assingment-2
+"""
 import random
 while(True):
     temp=random.randrange(0,100)
